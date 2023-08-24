cb33bca832df20450e974964c45e68f560707255
Chris Bras <chris.s.bras@gmail.com>
2023-08-24 13:59:08 +0200

    audio marking is now made using VAD

--- helpers/data.py
+++ helpers/data.py
@@ -1,15 +1,18 @@
 from mne.io import read_epochs_eeglab
 from scipy.io import wavfile
 from librosa import resample
+import numpy as np
 
 from helpers.processing import normalize
+from helpers.vad import get_speech_marking_for_file, VAD_SAMPLING_RATE
 
 DATA_LEN = 2048
 
 class DataException(Exception):
     pass
 
-def get_data(subject="sub-01", speech_type="covert", target="ee", epoch=0, eeg_nodes=[]):
+
+def get_data(subject="sub-01", speech_type="covert", target="ee", epoch=0, eeg_nodes=[], resampleRate=1024.0):
     eeg_data = read_epochs_eeglab(f"./data/derivatives/{subject}/eeg/{subject}_task-{speech_type}-{target}_eeg.set", verbose=False)
     df = eeg_data.to_data_frame()
     epoch_df = df[df["epoch"] == epoch]
@@ -27,9 +30,28 @@ def get_data(subject="sub-01", speech_type="covert", target="ee", epoch=0, eeg_n
         raise DataException(f"Invalid data size! Found length of {numpy_df.shape[0]} instead of required {DATA_LEN}")
 
     # get the audio data
-    rate, audio_data = wavfile.read(f"./data/sourcedata/{subject}/audio/{subject}_task-overt-{target}_run-{(epoch + 1):02d}_audio.wav")
-    
-    audio_data =  resample(audio_data / 2**31 , orig_sr=rate, target_sr=1024.0)
+    audio_path = f"./data/sourcedata/{subject}/audio/{subject}_task-overt-{target}_run-{(epoch + 1):02d}_audio.wav"
+    rate, audio_data = wavfile.read(audio_path)
+    markings = get_speech_marking_for_file(audio_path)
+
+
+
+    if resampleRate:
+        audio_data =  resample(audio_data / 2**31 , orig_sr=rate, target_sr=resampleRate)
+        # create marking array in resampled samplerate
+        start_speech = markings[0]['start'] / VAD_SAMPLING_RATE * resampleRate
+        start_speech = int(start_speech)
+        audio_marking = np.zeros_like(audio_data)
+        audio_marking[:start_speech] = 1
+  
+
+    else:
+        # create resampled marking in audio native rate
+        start_speech = markings[0]['start'] / VAD_SAMPLING_RATE * rate
+        start_speech = int(start_speech)
+
+        audio_marking = np.zeros_like(audio_data)
+        audio_marking[:start_speech] = 1
 
     audio_data = normalize(audio_data)
-    return numpy_df, audio_data
+    return numpy_df, audio_data, audio_marking
---
+++ helpers/vad.py
@@ -0,0 +1,31 @@
+VAD_SAMPLING_RATE = 16000
+
+import torch
+torch.set_num_threads(16)
+
+model, utils = torch.hub.load(repo_or_dir='snakers4/silero-vad',
+                              model='silero_vad',
+                              force_reload=True,
+                              onnx=False)
+
+(get_speech_timestamps,
+ save_audio,
+ read_audio,
+ VADIterator,
+ collect_chunks) = utils
+
+
+def get_speech_marking_for_file(filepath: str) -> dict:
+    """
+    Returns the beginning and end of speech for a given wave file
+    """
+    wav = read_audio(filepath, sampling_rate=VAD_SAMPLING_RATE)
+    speech_timestamps = get_speech_timestamps(wav, model, sampling_rate=VAD_SAMPLING_RATE)
+
+    return speech_timestamps
+
+if __name__ == "__main__":
+    PATH = "../data/sourcedata/sub-01/audio/sub-01_task-overt-aa_run-01_audio.wav"
+
+    timestamps = get_speech_marking_for_file(PATH)
+    print(timestamps)
